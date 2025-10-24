3dac635497ac8ce13124080cffd5825ad173dcd9
nouaoury <assianouaoury@gmail.com>
2025-10-24 22:09:09 +0100

    M

--- index.js
+++ index.js
@@ -31,10 +31,11 @@ return `Bonjour ${name}`;
 }
 const sayHelloArrow = (name) => `Bonjour ${name}`;
 
-console.log(sayHello('anas'))
-console.log(sayHelloArrow('mouquine'))
+console.log(sayHello('assia'))
+console.log(sayHelloArrow('nouaoury'))*/
 
-*/
+
+
 
 
 //exercice 4
@@ -130,7 +131,7 @@ console.log(user2.profile?.email);
 console.log(user2.address?.city); */
 
 
-const produits = [
+/*const produits = [
 { nom: "Lait", prix: 10, expireLe: "2025-12-01" },
 { nom: "Yaourt", prix: 5, expireLe: "2024-01-01" },
 { nom: "Jus", prix: 8, expireLe: "2026-02-15" },
@@ -139,4 +140,4 @@ const aujourdHui = new Date();
 const valides = produits.filter(p => new Date(p.expireLe) >aujourdHui);
 const total = valides.reduce((s, p) => s + p.prix, 0);
 console.log("Produits valides :", valides);
-console.log( "Total:  ", total, "DH");
+console.log( "Total:  ", total, "DH");*/
